ea368411e2d14f9306ca1294f34fdfda9d8fc038
David Lang <davidlang42@gmail.com>
2021-04-30 21:25:42 +1000

    Clean up imports

--- custom_components/template_climate/climate.py
+++ custom_components/template_climate/climate.py
@@ -9,75 +9,28 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.components.climate import (
     ClimateEntity,
     ENTITY_ID_FORMAT,
-    #TODO: do I need this? PLATFORM_SCHEMA,
+    PLATFORM_SCHEMA,
 )
-from homeassistant.components.climate.const import (#TODO: what of these do I actually need?
-    ATTR_PRESET_MODE,
-    CURRENT_HVAC_COOL,
-    CURRENT_HVAC_HEAT,
-    CURRENT_HVAC_IDLE,
-    CURRENT_HVAC_OFF,
-    HVAC_MODE_COOL,
-    HVAC_MODE_HEAT,
+from homeassistant.components.climate.const import (
+    HVAC_MODES,
     HVAC_MODE_OFF,
-    PRESET_AWAY,
-    PRESET_NONE,
-    SUPPORT_PRESET_MODE,
-    SUPPORT_TARGET_TEMPERATURE,
 )
-from homeassistant.const import ( #TODO: what of these do I actually need?
+from homeassistant.const import (
     CONF_ENTITY_ID,
     CONF_FRIENDLY_NAME,
     CONF_UNIQUE_ID,
     CONF_VALUE_TEMPLATE,
-    STATE_OFF,
-    STATE_ON,
     STATE_UNAVAILABLE,
     STATE_UNKNOWN,
-
-    ATTR_ENTITY_ID,
-    ATTR_TEMPERATURE,
-    CONF_NAME,
-    EVENT_HOMEASSISTANT_START,
-    PRECISION_HALVES,
-    PRECISION_TENTHS,
-    PRECISION_WHOLE,
-    SERVICE_TURN_OFF,
-    SERVICE_TURN_ON,
 )
 from .const import CONF_AVAILABILITY_TEMPLATE
 from .template_entity import TemplateEntity
-#TODO: what of these do I actually need?
 from homeassistant.exceptions import TemplateError
 from homeassistant.helpers.entity import async_generate_entity_id
 from homeassistant.helpers.script import Script
-# from homeassistant.core import DOMAIN as HA_DOMAIN, CoreState, callback
-# from homeassistant.exceptions import ConditionError
-# from homeassistant.helpers import condition
-# from homeassistant.helpers.event import (
-#     async_track_state_change_event,
-#     async_track_time_interval,
-# )
-# from homeassistant.helpers.reload import async_setup_reload_service
+from homeassistant.core import callback
 
 _LOGGER = logging.getLogger(__name__)
-
-#TODO: TEST SERVICES
-# climate.set_hvac_mode
-# climate.turn_on
-# climate.turn_off
-# availability
-
-#TODO: MVP SERVICES
-# climate.set_temperature
-# climate.set_fan_mode
-
-#TODO: FUTURE SERVICES
-# climate.set_aux_heat
-# climate.set_preset_mode
-# climate.set_humidity
-# climate.set_swing_mode
-# additional properties: https://developers.home-assistant.io/docs/core/entity/climate/
 
 CONF_CLIMATES = "climates"
 CONF_SET_HVAC_MODE_ACTION = "set_hvac_mode"
@@ -89,18 +42,7 @@ CLIMATE_SCHEMA = vol.Schema(
         vol.Required(CONF_VALUE_TEMPLATE): cv.template,
         vol.Optional(CONF_AVAILABILITY_TEMPLATE): cv.template,
         vol.Required(CONF_SET_HVAC_MODE_ACTION): cv.SCRIPT_SCHEMA,
-        vol.Optional(
-            CONF_HVAC_LIST,
-            default=[
-                HVAC_MODE_OFF,
-                HVAC_MODE_HEAT,
-                HVAC_MODE_COOL,
-                HVAC_MODE_HEAT_COOL,
-                HVAC_MODE_AUTO,
-                HVAC_MODE_DRY,
-                HVAC_MODE_FAN_ONLY,
-            ],
-        ): cv.ensure_list,#TODO: is this right?
+        vol.Optional(CONF_HVAC_LIST, default=HVAC_MODES): cv.ensure_list,#TODO: is this right?
         vol.Optional(CONF_ENTITY_ID): cv.entity_ids,
         vol.Optional(CONF_UNIQUE_ID): cv.string,
     }
